69863a4205af0f18983fa47119ff5c3ee44b4d89
Laura Rinehimer <rinehimerl@alumni.misericordia.edu>
2017-11-01 16:50:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOfArray(array, element){
   [element, ...array]
   return array
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift (element)
+}
